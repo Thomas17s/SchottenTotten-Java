4a04d432852cd6907b4292efd90238820f191918
Yanis550 <yanisprov1@gmail.com>
2025-12-23 23:08:28 +0100

    Update Carte.java

--- com/schottenTotten/model/Carte.java
+++ com/schottenTotten/model/Carte.java
@@ -2,7 +2,7 @@ package com.schottenTotten.model;
 
 public abstract class Carte implements Comparable<Carte> {
     
-    // Méthode abstraite : chaque enfant devra dire s'il est une tactique
+    // methode abstraite pour savoir si la carte est tactique
     public abstract boolean estTactique();
 
     public abstract Couleur getCouleur();
@@ -13,11 +13,11 @@ public abstract class Carte implements Comparable<Carte> {
 
     @Override
     public int compareTo(Carte autre) {
-        // Les cartes Tactiques sont mises à la fin de la main
+        // les cartes tactiques sont placees apres les cartes classiques
         if (this.estTactique() && !autre.estTactique()) return 1;
         if (!this.estTactique() && autre.estTactique()) return -1;
         
-        // Si même type, on trie par valeur
+        // si meme type de carte, tri par valeur
         return Integer.compare(this.getValeur(), autre.getValeur());
     }
 }
